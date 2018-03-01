b9eeebbdd4e3b33de690b3d7c420126177f945dc
ksritharan <kavin.sritharan@mail.utoronto.ca>
2018-03-01 13:09:29 -0500

    test

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret1'
 
+erver_data = {}
 
 @app.route('/', methods=['GET'])
 def index():
   #return render_template('index.html', users=User.query.all())
   return render_template('index.html')
 
-@socketio.on('my event')
-def test_message(message):
-  emit('my response', {'data': 'got it!'})
+@app.route('/data', methods=['POST'])
+def get_data():
+    print('Received data from client: %s' % request.data)
+    return Response('')
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
